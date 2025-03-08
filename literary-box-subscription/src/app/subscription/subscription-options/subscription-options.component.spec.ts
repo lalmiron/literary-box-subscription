7635546e82a567aaecaea9fef2af46cd8a957308
Laura Almirón <lauraandreaalmiron12@gmail.com>
2025-03-08 20:53:59 -0300

    add component subscription-options

---
+++ literary-box-subscription/src/app/subscription/subscription-options/subscription-options.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SubscriptionOptionsComponent } from './subscription-options.component';
+
+describe('SubscriptionOptionsComponent', () => {
+  let component: SubscriptionOptionsComponent;
+  let fixture: ComponentFixture<SubscriptionOptionsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SubscriptionOptionsComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SubscriptionOptionsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ literary-box-subscription/src/app/subscription/subscription-options/subscription-options.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscription-options',
+  imports: [],
+  templateUrl: './subscription-options.component.html',
+  styleUrl: './subscription-options.component.css'
+})
+export class SubscriptionOptionsComponent {
+
+}
